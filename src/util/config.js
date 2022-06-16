b41290ed90ca317e66eb7a8771370349af530e9f
Ben Sargent <ben@sgt.works>
2022-06-16 12:22:53 -0600

    - allow passing of config file on command line

--- src/util/config.js
+++ src/util/config.js
@@ -1,6 +1,8 @@
 // Dependencies
 import fs from 'fs'
 
-const config = JSON.parse(fs.readFileSync('./config.json'))
+const config = (if (process.argv[2])
+    ? JSON.parse(fs.readFileSync(process.argv[2]))
+        : JSON.parse(fs.readFileSync('./config.json'))
 // Module
 export default config
